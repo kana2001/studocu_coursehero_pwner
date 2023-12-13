bf9de10aafe30bfc57c93c030e7195e8d9072223
kana2001 <71416354+kana2001@users.noreply.github.com>
2023-12-12 21:23:03 -0500

    cleaned up script

--- pwner.py
+++ pwner.py
@@ -17,37 +17,18 @@ for chunk in stream:
         print(chunk.choices[0].delta.content, end="")
         essay_content += chunk.choices[0].delta.content
 
-
-# Get the directory path of the current script
+# essays will be saved in essays/
 script_directory = os.path.dirname(__file__)
-
-# Define the subfolder where you want to save the file
-subfolder = 'essays'  # Replace with the name of your subfolder
-
-# Create the full path to the subfolder within the script directory
+subfolder = 'essays'  
 directory = os.path.join(script_directory, subfolder)
-
-# Ensure the subfolder exists, create it if necessary
-if not os.path.exists(directory):
-    os.makedirs(directory)
-
-# Ensure the directory exists, create it if necessary
 if not os.path.exists(directory):
     os.makedirs(directory)
     
-# Split the essay content into lines
 lines = essay_content.strip().split('\n')
-
-# Extract the title from the first line
 title = lines[0].strip()  # Remove leading and trailing spaces
-
 # Replace any characters in the title that are not allowed in filenames
 title = "".join(c if c.isalnum() or c.isspace() else "_" for c in title)
-
-# Create a doc file with the extracted title as the filename
 file_name = f"{title}.doc"
-
-# Create the full path to the file
 file_path = os.path.join(directory, file_name)
 
 with open(file_path, "w") as file:
